d7d577950a0131146ebe12be12ca217bd6cbb551
Zeswen <zeswentyrael@gmail.com>
2019-01-13 13:49:20 +0100

    Debugging on 03

--- 03-cssvars/src/components/Controls/Controls.js
+++ 03-cssvars/src/components/Controls/Controls.js
@@ -5,8 +5,8 @@ import Control from '../Control';
 const Controls = (props) => (
     <div>
         {Object.keys(props.properties).map(property => (
-            <React.Fragment>
-                <Control key={property} property={property} value={props.properties[property]} changeProperty={props.changeProperty}/>
+            <React.Fragment key={property}>
+                <Control property={property} value={props.properties[property]} changeProperty={props.changeProperty}/>
             </React.Fragment>
         ))}
     </div>
